c8ac3e6915341e54bc6a6841cba3994a10374e53
rojjanasak <rojjanasak71@gmail.com>
2026-03-03 19:59:42 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,23 @@
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open("bakery-cache").then(cache=>{
-      return cache.addAll(["/","index.html"]);
-    })
+const CACHE_NAME = "kinnom-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(response=>{
-      return response || fetch(e.request);
-    })
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
